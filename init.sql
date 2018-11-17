86c3abe952944b2f8309f22299686d424936dffd
hi-watana <hero95alive@gmail.com>
2018-11-17 22:34:49 +0900

    Refined

--- init.sql
+++ init.sql
@@ -3,7 +3,7 @@ INSERT INTO `users` VALUES (NULL, 'Freddie', 'Password0')
     ,(NULL, 'Brian', 'gjaoihao0321')
     ,(NULL, 'John', 'tai4a0490a49_')
     ,(NULL, 'Rogger', '&&?las??A?');
-CREATE TABLE `items` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `user_id` INTEGER, `product_name` VARCHAR(255), `price` INTEGER);
+CREATE TABLE `items` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `user_id` INTEGER, `item_name` VARCHAR(255), `price` INTEGER);
 INSERT INTO `items` VALUES (NULL, 1, 'microphone', 100000)
     ,(NULL, 2, 'red special', 1000000)
     ,(NULL, 3, 'drum', 500000)
--- select.js
+++ select.js
@@ -1,3 +1,5 @@
+const { prompt } = require('enquirer');
+
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('', '', '', {
     dialect: 'sqlite',
@@ -5,46 +7,56 @@ const sequelize = new Sequelize('', '', '', {
     logging: false
 });
 
-var rl = require('readline').createInterface(process.stdin, process.stdout);
-
 new Promise(function(resolve, reject) {
-    rl.question('\033[1mLogin username:\033[0m ', function(username) {
-        rl.question('\033[1mPassword:\033[0m ', function(password) {
-            const User = sequelize.define('user', {
-                username: Sequelize.STRING,
-                password: Sequelize.STRING,
-            }, {
-                timestamps: false,
-            });
+    let question = [
+        {
+            type: 'input',
+            name: 'username',
+            message: 'Login username'
+        },
+        {
+            type: 'password',
+            name: 'password',
+            message: 'Password'
+        }
+    ];
+    prompt(question).then(answers => {
 
-            User.findAll({
-                attributes: ['id'],
-                where: {
-                    username: username,
-                    password: password,
-                }
-            }).then(res => {
-                if (res.length === 0) {
-                    console.log('\033[031mInvalid username or password\033[0m');
-                    process.exit(1);
-                }
-                resolve(res[0]['dataValues']['id']);
-            });
+        const User = sequelize.define('user', {
+            username: Sequelize.STRING,
+            password: Sequelize.STRING,
+        }, {
+            timestamps: false,
         });
-    })
+
+        User.findAll({
+            attributes: ['id'],
+            where: answers
+        }).then(res => {
+            if (res.length === 0) {
+                console.log('\033[031mInvalid username or password\033[0m');
+                process.exit(1);
+            }
+            resolve(res[0]['dataValues']['id']);
+        });
+    });
 }).then(function(user_id) {
-    new Promise(function(resolve, reject) {
-        rl.question('\033[1mSearch (product name):\033[0m ', function(product_name) {
-            resolve(product_name);
-        });
-    }).then(function(product_name) {
-        sequelize.query('SELECT product_name, price FROM `items` WHERE user_id = :user_id AND product_name LIKE :product_name', {
+    let question = [
+        {
+            type: 'input',
+            name: 'item_name',
+            message: 'Item name'
+        }
+    ];
+    prompt(question).then(answers => {
+        let item_name = answers['item_name'];
+        sequelize.query('SELECT item_name, price FROM `items` WHERE user_id = :user_id AND item_name LIKE :item_name', {
             replacements: {
                 user_id: [user_id],
-                product_name: [`%${product_name}%`]
+                item_name: [`%${item_name}%`]
             }
         }).spread((results, metadata) => {
-            console.log('\n\n', metadata, '\n\n');
+            console.log('\n\n', metadata, '\n\n', `\n${results.length} items found:`);
             results.forEach(i => console.log(i));
             process.exit(0);
         });
