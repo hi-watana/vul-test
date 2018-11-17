137219ed88b7afd47607f49ce8cfb0272e080b59
hi-watana <hero95alive@gmail.com>
2018-11-17 21:05:53 +0900

    Draft version.

---
+++ init.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `users` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `username` VARCHAR(255), `password` VARCHAR(255));
+INSERT INTO `users` VALUES (NULL, 'Freddie', 'Password0')
+    ,(NULL, 'Brian', 'gjaoihao0321')
+    ,(NULL, 'John', 'tai4a0490a49_')
+    ,(NULL, 'Rogger', '&&?las??A?');
+CREATE TABLE `items` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `user_id` INTEGER, `product_name` VARCHAR(255), `price` INTEGER);
+INSERT INTO `items` VALUES (NULL, 1, 'microphone', 100000)
+    ,(NULL, 2, 'red special', 1000000)
+    ,(NULL, 3, 'drum', 500000)
+    ,(NULL, 3, 'apple', 100)
+    ,(NULL, 4, 'electric base', 400000)
+    ,(NULL, 1, 'piano', 1000000)
+    ,(NULL, 4, 'pencil', 90)
+    ,(NULL, 2, 'dictionary', 5000)
+    ,(NULL, 2, 'telescope', 5000)
+    ,(NULL, 1, 'notebook', 100)
+    ,(NULL, 4, 'pen', 1000)
+    ,(NULL, 3, 'laptop', 200000)
--- select.js
+++ select.js
@@ -1,19 +1,53 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('', '', '', {
+    dialect: 'sqlite',
+    storage: 'database.sqlite3',
+    logging: false
+});
+
 var rl = require('readline').createInterface(process.stdin, process.stdout);
 
 new Promise(function(resolve, reject) {
-    rl.question('\033[1mSearch (product name):\033[0m ', function(product_name) {
-        resolve(product_name);
-    });
-}).then(function(product_name) {
-    const Sequelize = require('sequelize');
-    const sequelize = new Sequelize('sqlite:database.sqlite3');
+    rl.question('\033[1mLogin username:\033[0m ', function(username) {
+        rl.question('\033[1mPassword:\033[0m ', function(password) {
+            const User = sequelize.define('user', {
+                username: Sequelize.STRING,
+                password: Sequelize.STRING,
+            }, {
+                timestamps: false,
+            });
 
-    sequelize.query('SELECT * FROM `items` WHERE product_name LIKE :product_name', {
-        replacements: {
-            product_name: [`%${product_name}%`]
-        }
-    }).spread((results, metadata) => {
-        results.forEach(i => console.log(i));
-        process.exit(0);
+            User.findAll({
+                attributes: ['id'],
+                where: {
+                    username: username,
+                    password: password,
+                }
+            }).then(res => {
+                if (res.length === 0) {
+                    console.log('\033[031mInvalid username or password\033[0m');
+                    process.exit(1);
+                }
+                resolve(res[0]['dataValues']['id']);
+            });
+        });
+    })
+}).then(function(user_id) {
+    new Promise(function(resolve, reject) {
+        rl.question('\033[1mSearch (product name):\033[0m ', function(product_name) {
+            resolve(product_name);
+        });
+    }).then(function(product_name) {
+        sequelize.query('SELECT product_name, price FROM `items` WHERE user_id = :user_id AND product_name LIKE :product_name', {
+            replacements: {
+                user_id: [user_id],
+                product_name: [`%${product_name}%`]
+            }
+        }).spread((results, metadata) => {
+            console.log('\n\n', metadata, '\n\n');
+            results.forEach(i => console.log(i));
+            process.exit(0);
+        });
     });
 });
+
