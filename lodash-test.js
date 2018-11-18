d3ec8c1c921f1bd52770e0990f26074a5a810604
hi-watana <hero95alive@gmail.com>
2018-11-18 13:57:38 +0900

    Modified

--- lodash-test.js
+++ lodash-test.js
@@ -1,11 +1,13 @@
-var _ = require('lodash');
+const _ = require('lodash');
 function _string_escape(val, timeZone, dialect, format) {
     return [val, timeZone, dialect, format];
 }
 
-var string_escape = _.partial(_string_escape, _, null, 'sqlite', true);
-console.log('partial\t\t', string_escape("%'--%", 0, ["%'--%"]));
+const args = ["%'--%", 0, ["%'--%"]];
 
-var string_escape = _.partialRight(_string_escape, _, null, 'sqlite', true);
-console.log('partialRight\t', string_escape("%'--%", 0, ["%'--%"]));
+var string_escape = _.partial(_string_escape, _, null, 'sqlite', true);
+console.log('partial\t\t', string_escape(...args));
+
+var string_escape = _.partialRight(_string_escape, null, 'sqlite');
+console.log('partialRight\t', string_escape(...args));
 
